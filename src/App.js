37fcd7583b3f5909fd8b971e477c6ac0491278a1
RatchagreeA <ratchagree@gmail.com>
2022-07-07 22:31:59 +0700

    create state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import "./App.scss";
 import { useStete } from "react";
 
 function App() {
+    const [brkLength, setBrkLength] = useStete("5");
+    const [seshLength, setSeshLength] = useStete("25");
+    const [timerState, setTimerState] = useStete("stopped");
+    const [timerType, setTimerType] = useStete("Session");
+    const [timer, setTimer] = useStete(1500);
+    const [intervalID, setIntervalID] = useStete("");
     return (
         <div className="App">
             <div className="app-container">
                 <div className="main-title">25 + 5 Clock</div>
                 <div className="control-container">
-                    {" "}
-                    <TimeControl />
-                    <TimeControl />
+                    <TimeControl
+                        titleID="break-label"
+                        title="Break Length"
+                        minID="break-decrement"
+                        addID="break-increment"
+                        lengthID="break-length"
+                        length={brkLength}
+                        onClick=""
+                    />
+                    <TimeControl
+                        titleID="session-label"
+                        title="session Length"
+                        minID="session-decrement"
+                        addID="session-increment"
+                        lengthID="session-length"
+                        length={seshLength}
+                        onClick=""
+                    />
                 </div>
                 <div className="timer">
                     <div className="timer-wrapper">
@@ -39,25 +60,33 @@ function App() {
 
 export default App;
 
-function TimeControl() {
+function TimeControl({
+    titleID,
+    title,
+    minID,
+    addID,
+    lengthID,
+    length,
+    onClick,
+}) {
     return (
         <div className="length-control">
-            <div id={"this.props.titleID"}>{"this.props.title"}</div>
+            <div id={titleID}>{title}</div>
             <button
                 className="btn-level"
-                id={"this.props.minID"}
-                onClick={"this.props.onClick"}
+                id={minID}
+                onClick={onClick}
                 value="-"
             >
                 <i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i>
             </button>
-            <div className="btn-level" id={"this.props.lengthID"}>
-                {"this.props.length"}
+            <div className="btn-level" id={lengthID}>
+                {length}
             </div>
             <button
                 className="btn-level"
-                id={"this.props.addID"}
-                onClick={"this.props.onClick"}
+                id={addID}
+                onClick={onClick}
                 value="+"
             >
                 <i class="fa fa-arrow-circle-o-up" aria-hidden="true"></i>
